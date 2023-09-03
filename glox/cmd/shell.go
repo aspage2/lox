4f56b414f516e401272d406548e463ec59dc82d5
Alex Page <apage@mintel.com>
2023-09-03 12:07:05 -0500

    readline

--- glox/cmd/shell.go
+++ glox/cmd/shell.go
@@ -1,41 +1,46 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"glox/runtime"
 	"io"
-	"os"
 	"strings"
+
+	"github.com/chzyer/readline"
 )
 
-func startUp() {
+func startUpMessage() {
 	fmt.Println("\u001b[38;2;128;204;204m", logo, "\u001b[0m")
 	fmt.Println("\n  Interactive Shell \u001b[36m" + version + "\u001b[0m")
 	fmt.Println("  (\u001b[32mCtrl+d or :q to exit\u001b[0m)")
 }
 
-func interactiveShell(l *runtime.Lox) {
-	startUp()
+func goodbyeMessage() {
+	fmt.Println("Goodbye.")
+}
 
-	goodByeMessage := func() {
-		fmt.Println("Goodbye.")
+func interactiveShell(l *runtime.Lox) {
+	startUpMessage()
+
+	rl, err := readline.New(">>> ")
+	if err != nil {
+		panic(err)
 	}
-	reader := bufio.NewReader(os.Stdin)
+	defer rl.Close()
 
 	for {
 		fmt.Print(">>> ")
-		data, err := reader.ReadString('\n')
+		data, err := rl.Readline()
 		if err != nil {
 			if err == io.EOF {
-				goodByeMessage()
+				goodbyeMessage()
 				return
 			}
 			panic(err)
 		}
 		data = strings.TrimSpace(data)
 		if data == ":q" {
-			goodByeMessage()
+			goodbyeMessage()
 			return
 		}
 
--- glox/go.mod
+++ glox/go.mod
@@ -3,9 +3,11 @@ module glox
 go 1.18
 
 require (
+	github.com/chzyer/readline v1.5.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
+	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
